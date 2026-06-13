1fa29887f2f9e752284d8b168fdfc58fd68c9406
anishshobithps <anish.shobith19@gmail.com>
2026-06-14 04:08:33 +0530

    posthog: add pageviews

--- src/components/posthog-provider.tsx
+++ src/components/posthog-provider.tsx
@@ -1,9 +1,13 @@
 'use client';
 
 import posthog from 'posthog-js';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { useRouter } from 'waku/router/client';
 
 export function PostHogProvider() {
+  const { path } = useRouter();
+  const isInitial = useRef(true);
+
   useEffect(() => {
     posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
       api_host: import.meta.env.VITE_POSTHOG_HOST,
@@ -11,6 +15,14 @@ export function PostHogProvider() {
       capture_pageleave: true,
     });
   }, []);
+
+  useEffect(() => {
+    if (isInitial.current) {
+      isInitial.current = false;
+      return;
+    }
+    posthog.capture('$pageview', { $current_url: window.location.href });
+  }, [path]);
 
   return null;
 }
